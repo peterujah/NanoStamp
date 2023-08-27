83bb96a051975fee1d426f19132f6a4f33744940
Chigozie Peter <ujahchigozie@gmail.com>
2023-08-28 00:46:25 +0800

    Create Stamp.php

---
+++ src/Stamp.php
@@ -0,0 +1,75 @@
+<?php 
+namespace NanoBlockTech\Stamp;
+use \NanoBlockTech\Stamp\Colors;
+use \NanoBlockTech\Stamp\Circle;
+use \NanoBlockTech\Stamp\Square;
+use \NanoBlockTech\Stamp\Exception\StampException;
+use \NanoBlockTech\Stamp\Exception\StampArgumentException;
+
+class Stamp {
+    /** 
+    * Hold stamp gd image container
+    * @var resource $image
+    */
+    private $image;
+
+    /** 
+    * Hold stamp container class
+    * @var object|Circle|Square $container
+    */
+    private $container;
+
+    /** 
+    * Hold stamp class version
+    * @var string static::VERSION
+    */
+    public const VERSION = '1.0.0';
+    
+
+    /** 
+    * Initialize stamp
+    * @param object|Circle|Square $container
+    */
+    public function __construct(object $container) {
+        if (!($container instanceof Circle) && !($container instanceof Square)) {
+            throw new StampArgumentException("Invalid container instance. It should be an instance of Circle or Square.");
+        }
+        $this->container = $container;
+        $this->image = imagecreatetruecolor($this->container->getWidth(), $this->container->getHeight());
+        if ($this->image === false) {
+            throw new StampException("Image creation failed.");
+        }
+        $this->setBackgroundTransparent();
+        $this->container->setImage($this->image);
+        $this->container->setVersion(self::VERSION);
+        
+    }
+
+    /** 
+    * Set stamp image container transparent background
+    */
+    public function setBackgroundTransparent() {
+        imagesavealpha($this->image, true);
+        $color = Colors::ALPHA_TRANSPARENT;
+        $transparentColor = imagecolorallocatealpha($this->image, $color[0], $color[1], $color[2], $color[3]);
+        if ($transparentColor === false) {
+            throw new StampException("Transparent color allocation failed.");
+        }
+        imagefill($this->image, 0, 0, $transparentColor);
+    }
+
+    /** 
+    * Get stamp image container class instance
+    * @return object|Circle|Square $this->container
+    */
+    public function getContainerInstance(){
+        return $this->container;
+    }
+
+    /** 
+    * Destroy stamp image resource
+    */
+    public function __destruct() {
+        imagedestroy($this->image);
+    }
+}
