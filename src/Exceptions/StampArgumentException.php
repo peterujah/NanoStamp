e44c8180410d57c5ea0bf7223c46e70d3ec7f2df
Chigozie Peter <ujahchigozie@gmail.com>
2023-08-28 00:55:40 +0800

    Update StampArgumentException.php

--- src/Exceptions/StampArgumentException.php
+++ src/Exceptions/StampArgumentException.php
@@ -1,5 +1,10 @@
 <?php 
-namespace NanoBlockTech\Stamp\Exception;
+/**
+ * @author      Peter Chigozie(NG) peterujah
+ * @copyright   Copyright (c), 2023 Peter(NG) peterujah
+ * @license     MIT public license
+ */
+namespace Peterujah\NanoBlockTech\Stamp\Exception;
 class StampArgumentException extends \InvalidArgumentException {
     public function __construct($message = "Invalid argument exception", $code = 0, Throwable $previous = null) {
         parent::__construct($message, $code, $previous);
