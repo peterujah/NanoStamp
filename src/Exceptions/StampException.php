790b776f2ebfa8d9cd916d55851252b9f1c8a597
Chigozie Peter <ujahchigozie@gmail.com>
2023-08-28 00:55:23 +0800

    Update StampException.php

--- src/Exceptions/StampException.php
+++ src/Exceptions/StampException.php
@@ -1,5 +1,10 @@
 <?php 
-namespace NanoBlockTech\Stamp\Exception;
+/**
+ * @author      Peter Chigozie(NG) peterujah
+ * @copyright   Copyright (c), 2023 Peter(NG) peterujah
+ * @license     MIT public license
+ */
+namespace Peterujah\NanoBlockTech\Stamp\Exception;
 class StampException extends \Exception {
     public function __construct($message = "Image creation failed.", $code = 0, Throwable $previous = null) {
         parent::__construct($message, $code, $previous);
